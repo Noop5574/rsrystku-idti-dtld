405aa30e37cd91313e88f64e749140d505429d22
Noop5574 <44319316+Noop5574@users.noreply.github.com>
2018-10-24 22:54:02 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,84 +1,32 @@
-const Discord = require('discord.js');                                                                                                                                                                                                                                                                                                                                                                                                                          /*جميع حقوق هذا الملف محفوظه*/                                                     
+const Discord = require("discord.js");
 const client = new Discord.Client();
-
-
-
-
-
-
-
-
-
-
-
-
-
-// جميع حقوق هذا الملف محفوظة ,
-client.login(process.env.BOT_TOKEN);// هنا تضع توكان البوت الخاص بك
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const client = new Discord.Client();
-client.on('ready', () => {
-  client.user.setGame(`!!bc | OP SHOP`,'https://www.twitch.tv/ProGamer44');
-  console.log('Im Ready!');
-});
-/*ProGamer44*/
-client.on('message', message => {
-if (message.content.split(' ')[0] == '!!bc')
- message.guild.members.forEach( member => {
-         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
-		 if(!message.channel.guild) return message.reply('** This command only for servers **');
-member.send( `${member} ! ` + "**" + message.guild.name + " : ** " + message.content.substr(3));
-                                                            message.delete();
-});
-});
+var prefix = "!!";
 client.on("message", message => {
-    var prefix = "!!";
- 
-            var args = message.content.substring(prefix.length).split(" ");
+
             if (message.content.startsWith(prefix + "bc")) {
                          if (!message.member.hasPermission("ADMINISTRATOR"))  return;
-						 if(!message.channel.guild) return message.reply('** This command only for servers **');
- if (!args[1]) {
-                                let embed3 = new Discord.RichEmbed()
-                                .setDescription(":white_check_mark: | تم ارسال للكل رساله فارغه")
-                                .setColor("RANDOM")
-                                message.channel.sendEmbed(embed3);
-                            } else {
-
-                            let embed4 = new Discord.RichEmbed()
-                                                            .setDescription(':white_check_mark: | تم ارسال للكل الرساله !')
-                                                            .setColor("#008000")
-                                message.channel.sendEmbed(embed4);
-                                                      message.delete();
-                            }
-                          }
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' '); 
+  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+ m.send(`${argresult}\n ${m}`);
+})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`); 
+ message.delete(); 
+};     
 });
+
+
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Desert Bot- Script By : i1Suhaib`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`#bc | osama tech`,"http://twitch.tv/S-F")
+client.user.setStatus("dnd")
+});
+
+
+
 client.login(process.env.BOT_TOKEN);
