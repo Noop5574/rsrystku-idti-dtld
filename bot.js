0114d9a6caff884e1d053b043ea01d30624c5ff2
Noop5574 <44319316+Noop5574@users.noreply.github.com>
2018-10-24 22:43:23 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,49 +1,84 @@
-const Discord = require("discord.js");
-  const kboosh = new Discord.Client();
-    const kbooshtoken = "MzI3NzM2NTMyOTMwOTIwNDUw.DGmnLA.QFbtYXPzTAvZsoFW70wNLyVm3lE"
-      kboosh.on('ready', () => {
-        kboosh.user.setGame(`!!bc |OP SHOP`,'https://www.twitch.tv/TEST-Broadcast');
-          console.log('Im Ready!');
-  
-        });
+const Discord = require('discord.js');                                                                                                                                                                                                                                                                                                                                                                                                                          /*جميع حقوق هذا الملف محفوظه*/                                                     
+const client = new Discord.Client();
 
-  kboosh.on('message', message => {
-    if (message.content.split(' ')[0] == '%k')
-       message.guild.members.forEach( member => {
+
+
+
+
+
+
+
+
+
+
+
+
+// جميع حقوق هذا الملف محفوظة ,
+client.login(process.env.BOT_TOKEN);// هنا تضع توكان البوت الخاص بك
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const client = new Discord.Client();
+client.on('ready', () => {
+  client.user.setGame(`!!bc | OP SHOP`,'https://www.twitch.tv/ProGamer44');
+  console.log('Im Ready!');
+});
+/*ProGamer44*/
+client.on('message', message => {
+if (message.content.split(' ')[0] == '!!bc')
+ message.guild.members.forEach( member => {
          if (!message.member.hasPermission("ADMINISTRATOR"))  return;
-
-
-           member.send( `${member} ! ` + "**" + message.guild.name + " : ** " + message.content.substr(3));
-                                                      message.delete();
-            
-                                                    });
-            
-                                                  });
-   kboosh.on("message", message => {
-       var prefix = "!!";
+		 if(!message.channel.guild) return message.reply('** This command only for servers **');
+member.send( `${member} ! ` + "**" + message.guild.name + " : ** " + message.content.substr(3));
+                                                            message.delete();
+});
+});
+client.on("message", message => {
+    var prefix = "!!";
  
-             var args = message.content.substring(prefix.length).split(" ");
-                if (message.content.startsWith(prefix + "b")) {
-                          if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+            var args = message.content.substring(prefix.length).split(" ");
+            if (message.content.startsWith(prefix + "bc")) {
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+						 if(!message.channel.guild) return message.reply('** This command only for servers **');
+ if (!args[1]) {
+                                let embed3 = new Discord.RichEmbed()
+                                .setDescription(":white_check_mark: | تم ارسال للكل رساله فارغه")
+                                .setColor("RANDOM")
+                                message.channel.sendEmbed(embed3);
+                            } else {
 
-                          if (!args[1]) {
-                            
-                                 let embed3 = new Discord.RichEmbed()
-                                     .setDescription(":white_check_mark: | تم ارسال رسالة لا يوجد فيها شيء")
-                                       .setColor("#FF00FF")
-                                          message.channel.sendEmbed(embed3);
-                            
-                                        } else {
-
-                            
-                                           let embed4 = new Discord.RichEmbed()
-                                                            .setDescription(':white_check_mark: | تم ارسال الرساله للجميع ..')
-                                                                .setColor("#99999")
-                               
-                                                                message.channel.sendEmbed(embed4);
+                            let embed4 = new Discord.RichEmbed()
+                                                            .setDescription(':white_check_mark: | تم ارسال للكل الرساله !')
+                                                            .setColor("#008000")
+                                message.channel.sendEmbed(embed4);
                                                       message.delete();
                             }
                           }
 });
-
 client.login(process.env.BOT_TOKEN);
